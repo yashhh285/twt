195b3b07fb5fe8c5eb3f41dfa172c97d882cd054
yashhh285 <yashkothari285@gmail.com>
2025-02-09 19:39:43 -0600

    Update tweeter.py

--- tweeter.py
+++ tweeter.py
@@ -1,23 +1,26 @@
-import streamlit as st
 import tweepy
 from textblob import TextBlob
 
-# Authentication credentials (ensure these are securely managed)
+# Authentication credentials
 consumer_key = 'YOUR_CONSUMER_KEY'
 consumer_secret = 'YOUR_CONSUMER_SECRET'
 access_token = 'YOUR_ACCESS_TOKEN'
 access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
 
-# Set up authentication and API object
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+# Set up authentication
+client = tweepy.Client(
+    bearer_token="YOUR_BEARER_TOKEN", 
+    consumer_key=consumer_key, 
+    consumer_secret=consumer_secret, 
+    access_token=access_token, 
+    access_token_secret=access_token_secret
+)
 
 # Function to fetch tweets and analyze sentiment
 def fetch_tweets(query):
-    tweets = api.search(q=query, lang='en', count=100)
+    tweets = client.search_recent_tweets(query=query, max_results=100, tweet_fields=["text"])
     positive, negative, neutral = 0, 0, 0
-    for tweet in tweets:
+    for tweet in tweets.data:
         blob = TextBlob(tweet.text)
         sentiment = blob.sentiment.polarity
         if sentiment > 0:
@@ -27,13 +30,3 @@ def fetch_tweets(query):
         else:
             neutral += 1
     return positive, negative, neutral
-
-# Streamlit UI
-st.title("Real-Time Brand Sentiment Analysis")
-query = st.text_input("Enter a brand name or hashtag:")
-if query:
-    pos, neg, neut = fetch_tweets(query)
-    st.write(f"Positive Tweets: {pos}")
-    st.write(f"Negative Tweets: {neg}")
-    st.write(f"Neutral Tweets: {neut}")
-    st.bar_chart([pos, neg, neut], use_container_width=True)
